38c515a2dafc2bff7feb0851afdecd688ac6615e
Pedro Neves <pedro.hfn7@gmail.com>
2023-01-08 02:19:39 +0100

    Update Web API for fetching code

--- screeps-apis/web-api.js
+++ screeps-apis/web-api.js
@@ -5,7 +5,26 @@ const HOST = 'screeps.com';
 
 const getOrigin = () => `${PROTO}://${HOST}`;
 
-export const getCode = async (token = '') => {
+export const fetchCode = async (token = '') => {
+  if (!token) throw new Error('Missing token');
+
+  const searchParams = new URLSearchParams({ _token: token });
+
+  const url = new URL('/api/user/code', getOrigin());
+  url.search = searchParams.toString();
+
+  const response = await fetch(url, {
+    method: 'GET',
+  });
+
+  if (response.ok) return response.json();
+
+  const failedError = new Error('Failed to fetch code');
+  failedError.response = response;
+  throw failedError;
+};
+
+export const sendCode = async (token, data) => {
   if (!token) throw new Error('Missing token');
 
   const searchParams = new URLSearchParams({ _token: token });
@@ -17,7 +36,7 @@ export const getCode = async (token = '') => {
   headers.set('Content-Type', 'application/json; charset=utf-8');
 
   const response = await fetch(url, {
-    method: 'GET',
+    method: 'POST',
     headers,
   });
 
