09d69e29266abafb9f118dfe19b1f9ee99e9b776
Pedro Neves <pedro.hfn7@gmail.com>
2023-01-26 13:33:04 -0300

    Update main to run the miner role

--- src/dist/default/main.js
+++ src/dist/default/main.js
@@ -1,7 +1,13 @@
 const roleSpawn = require('./role.spawn');
+const roleMiner = require('./role.miner');
 
 module.exports.loop = function () {
   for (let spawnName in Game.spawns) {
     roleSpawn.run(spawnName);
+  }
+
+  for (let creepName in Game.creeps) {
+    const creep = Game.creeps[creepName];
+    if (creep.memory && creep.memory.role === 'miner') roleMiner.run(creep);
   }
 };
